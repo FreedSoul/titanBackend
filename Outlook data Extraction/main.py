a383c643aa9fed4ef2c0ee7f2c4031441f2cd565
mikeflo94 <mike.flo@outlook.com>
2023-11-08 12:26:57 -0500

    script successfully output the csv

--- Outlook data Extraction/main.py
+++ Outlook data Extraction/main.py
@@ -1,6 +1,9 @@
 import win32com.client
-from bs4 import BeautifulSoup
 import pandas as pd
+from io import StringIO
+import numpy as np
+import os
+from bs4 import BeautifulSoup
 
 # Getting message content from last received email on the inbox
 outlook = win32com.client.Dispatch('Outlook.Application').GetNamespace('MAPI')
@@ -9,35 +12,42 @@ inbox = default_inbox.Items
 last_message = inbox.GetLast()
 html_message = last_message.HTMLBody
 
-# Getting parser table
+# converts the HTML table to a list
+file = StringIO(html_message)
+table = pd.read_html(file)
 
-
-# getting headers from the HTML file
+# get headers and the number of headers of the list
 list_header = []
 soup = BeautifulSoup(html_message, 'html.parser')
 header = soup.find_all('table')[0].find('tr')
 
 for items in header:
-    try:
-        list_header.append(items.get_text())
-    except:
-        continue
+    list_header.append(items.get_text())
 
-# Getting data
-data = []
-html_data = soup.find_all('table')[0].find_all('tr')[1:]
+columns = len(list_header)
 
-for element in html_data:
-    sub_data = []
-    for sub_element in element:
-        try:
-            sub_data.append(sub_element.get_text())
-        except:
-            continue
-    data.append(sub_data)
+# get the number of rows of the list
+row = [len(x) for x in table][0]
 
-# creating dataframe
-dataFrame = pd.DataFrame(data=data, columns=list_header)
+# Converts list to np.array then to a Pandas data frame
+num = np.array(table)
+reshaped = num.reshape(row, columns)
+df = pd.DataFrame(reshaped)
 
-# Converting to CSV
-dataFrame.to_csv('Output.csv')
+# prints to console the data frame and saves file as .csv
+df.to_csv('output.csv', header=False, index=False)
+
+# Read the CSV file
+df = pd.read_csv('output.csv')
+
+# Remove rows with empty values in the specified columns
+df.dropna(subset=['AGGREGATE SOURCE', 'QTY',], inplace=True)
+
+# Write the cleaned dataframe to a new CSV file
+df.to_csv('final_output.csv', index=False)
+
+# delete first output file
+if os.path.exists("output.csv"):
+    os.remove("output.csv")
+else:
+    print("The file does not exist")
