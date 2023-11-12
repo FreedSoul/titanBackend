d4c118181ac6b0c0d177109f92edd4667a799ceb
FreedSoul <mattwork07@gmail.com>
2023-11-11 20:50:25 -0500

    fix to build #2

--- src/app/api/tablesData/route.ts
+++ src/app/api/tablesData/route.ts
@@ -1,16 +1,16 @@
 
-import fs from 'fs'
-import { type NextRequest, type NextResponse } from 'next/server';
-import path from 'path';
-import fsPromises from 'fs/promises';
+// import fs from 'fs'
+// import { type NextRequest, type NextResponse } from 'next/server';
+// import path from 'path';
+// import fsPromises from 'fs/promises';
 
-// Specify the path to your JSON file
-export default async function getTableData() {
+// // Specify the path to your JSON file
+// export default async function getTableData() {
   
-  const filePath = path.join(process.cwd(), './Outlook data Extraction/json_output.json');
-  let data: string = await fsPromises.readFile(filePath, 'utf8')
-  let jsonData = JSON.parse(data)
-  // console.log(jsonData, 'jsnDATA')
-  // if(jsonData){} 
-  return jsonData
-}
+//   const filePath = path.join(process.cwd(), './Outlook data Extraction/json_output.json');
+//   let data: string = await fsPromises.readFile(filePath, 'utf8')
+//   let jsonData = JSON.parse(data)
+//   // console.log(jsonData, 'jsnDATA')
+//   // if(jsonData){} 
+//   return jsonData
+// }
